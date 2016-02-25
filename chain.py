ece08fc3d9a46fe2c176b7a2e867eb22068779f2
Daimonie <daimonie@users.noreply.github.com>
2016-02-25 13:33:17 +0100

    Added argument parser. Will now test it on home PC. Afterwards, Will test if I can sync back to GetDataJoy. If so, I will parallelise it at the home computer, push to github repository and then sync to datajoy.

--- chain.py
+++ chain.py
@@ -1,9 +1,18 @@
+#Numeric Python. Should be familiar.
 import numpy as np
+#Plotting tools
 import matplotlib
 import matplotlib.pyplot as plt
+#Our calculation module
 from igf import *
+#sys. sys.stderr is a nice text stream.
 import sys as sys
+#Command line arguments.
+import argparse as argparse  
+#Time, because I want to see the total calculation time in ms.
 import time
+#Start the clock.
+
 global_time_start = time.time()
 #two level system, to start with
 #this is the simplest couloumb-blockade system around
@@ -12,20 +21,61 @@ global_time_start = time.time()
 #   resources, i.e. a computer that should've been retired years ago, I a
 #   working online (Get Data Joy) and can't use commandline arguments.   
 plotting_mode = 2
-chain_length = 4
-
-dot_levels = 2* chain_length #All at the same energy; this is a chain.
+chain_length = 2
 
 capacitive_strength = 00.
 tunnel_strength = 0.5
 epsilon_gap = .15
-epsilon_left = -5.0
-epsilon_right = 5.0
-resolution = 100
-gamma_strength = 0.05 
-param_beta = 0.05 * 50
 
+parser	= argparse.ArgumentParser(prog="N-chain",
+  description = "Calculates tranmission or spectral function through a chain of N elements.")  
+  
+parser.add_argument(
+    '-m',
+    '--mode',
+    help='Plotting Mode. Zero is transmission, 1 spectral, 2 saves transmission to a svg, 3 does the same for spectral.',
+    action='store',
+    type = int,
+    default = plotting_mode
+)   
+parser.add_argument(
+    '-n',
+    '--chain',
+    help='Plotting Mode. Zero is transmission, 1 spectral, 2 saves transmission to a svg, 3 does the same for spectral.',
+    action='store',
+    type = int,
+    default = chain_length
+)   
+parser.add_argument( 
+    '--cstr',
+    help='Plotting Mode. Zero is transmission, 1 spectral, 2 saves transmission to a svg, 3 does the same for spectral.',
+    action='store',
+    type = float,
+    default = capacitive_strength
+)   
+parser.add_argument( 
+    '--tstr',
+    help='Plotting Mode. Zero is transmission, 1 spectral, 2 saves transmission to a svg, 3 does the same for spectral.',
+    action='store',
+    type = float,
+    default = tunnel_strength
+)   
+parser.add_argument( 
+    '--eps',
+    help='Plotting Mode. Zero is transmission, 1 spectral, 2 saves transmission to a svg, 3 does the same for spectral.',
+    action='store',
+    type = float,
+    default = epsilon_gap
+)   
+args	= parser.parse_args() 
 
+plotting_mode = args.mode
+chain_length = args.chain
+capacitive_strength = args.cstr
+tunnel_strength = args.tstr
+epsilon_gap = args.eps
+
+dot_levels = 2* chain_length #All at the same energy; this is a chain.
 
 param_u = np.zeros((dot_levels, dot_levels))
 param_tau = np.zeros((dot_levels, dot_levels))
@@ -49,16 +99,22 @@ for i in range(0, dot_levels):
 print param_tau
 print param_u
 param_epsilon = np.diag( np.ones((dot_levels)))
+epsilon_left = -5.0
+epsilon_right = 5.0
+resolution = 100
 
 #This makes coupling to the leads
 #comparable to the coupling between levels. 
+gamma_strength = 0.05 
 
 param_gamma_left = np.zeros((dot_levels,dot_levels))
 param_gamma_right = np.zeros((dot_levels,dot_levels))
 
 param_gamma_left[0][0] = gamma_strength;
 param_gamma_right[epsilon_gap][epsilon_gap] = gamma_strength;
- 
+
+#Inverse temperature (units same as the others)
+param_beta = 0.05 * 50
 
 calculation = igfwl(
     param_epsilon, 
