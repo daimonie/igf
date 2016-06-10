1439956a0611ab6900f394f4551a5089edd72ae3
daimonie <daimonie@Atlantis.Atlantis>
2016-06-10 17:22:22 +0200

    Attempts at analysis are still failing.
    Neither the [chance] nor the [phase] nor the [energy] overview shows any feature that is evidently the reason of the triangle.
    
    So confusing.

--- perrin_energy_parallel.py
+++ perrin_energy_parallel.py
@@ -55,24 +55,15 @@ parser.add_argument(
     type = float,
     default = 1.0
 )   
-parser.add_argument(
-    '-i',
-    '--state',
-    help='Many-body state whose eigenvalues we want to see',
-    action='store',
-    type = int,
-    default = 0
-)   
 args    = parser.parse_args() 
  
 cores = args.cores
 param_type = args.param
 ksi = args.ksi
 zeta = args.zeta
-state = args.state
 
 ###
-bias_left = 0.00
+bias_left = -1.00
 bias_right = 1.00
 bias_res = 100
  
@@ -93,11 +84,11 @@ tick_max = 'auto'
 tick_min = 'auto'
 #tick_min = -tick_max
 
-cmap = plt.get_cmap('afmhot') 
+cmap = plt.get_cmap('winter') 
 ###
 
-def calculate_argument(arguments): 
-    print "Argument, arguments", arguments, "\n\n"
+def calculate_energy(arguments): 
+    #print "Argument, arguments", arguments, "\n\n"
     epsilon_res = 250
     biaswindow  = arguments[0]
     alpha       = arguments[1]
@@ -108,11 +99,8 @@ def calculate_argument(arguments):
     levels      = arguments[6] 
     ksi         = arguments[7] 
     zeta        = arguments[8] 
-    state_to_look_at = arguments[9]
-    
-    print "Look at state %d" % state_to_look_at
-    angle_plus = []
-    angle_minu = []
+     
+    energy_vector = [] 
     
     for bias in biaswindow:
         
@@ -152,20 +140,33 @@ def calculate_argument(arguments):
             beta
         )
         
-        state = calculation.ket(state_to_look_at)
-        capacitive_contribution = np.dot(state.T, np.dot( interaction, state))
+        superset = calculation.generate_superset(0) 
+        chances = calculation.distribution()
         
-        angle_plus.append(levels + capacitive_contribution + 0.5*alpha * bias + 1j*gamma)
-        angle_minu.append(levels + capacitive_contribution - 0.5*alpha * bias + 1j*gamma)    
-        
-    angle_plus = np.angle(angle_plus)
-    angle_minu = np.angle(angle_minu)
-    return [biaswindow, angle_plus, angle_minu]
+        this_energy = 0.00
+        for i in superset:
+            if chances[i] > 1e-3:
+                state           = calculation.ket(i)
+                #print i, chances[i], state
+                norm_squared    = np.dot(state.T, state)
+                
+                if norm_squared > 0:
+                    energy          = np.dot(state.T, np.dot( calculation.epsilon, state))
+                    interaction     = 0.00
+                    
+                    if state[0] == 1 and state[1] == 1:
+                        interaction += float("Inf")
+                    
+                    this_energy = energy + interaction 
+                    print "%d\t%.3e\t%d-%d\t%.3f"% (i, chances[i], state[0], state[1], energy + interaction) 
+                    this_energy *= chances[i]
+        energy_vector.append(this_energy)
+    return [biaswindow, energy_vector]
 
 biaswindow = np.linspace(bias_left, bias_right, bias_res) 
 param_space = np.linspace(param_left,param_right,param_res)
 
-manager = taskManager( cores, calculate_argument ) 
+manager = taskManager( cores, calculate_energy ) 
 #manager = taskManager( cores, ana_current ) 
 #print "Using analytic current..."
 for param in param_space: 
@@ -193,47 +194,37 @@ for param in param_space:
         ksi = param 
     elif param_type == 'z':
         zeta = param  
-    manager.add_params([biaswindow, alpha, tau, gamma, capacitive, beta, levels, ksi, zeta, state]) 
+    manager.add_params([biaswindow, alpha, tau, gamma, capacitive, beta, levels, ksi, zeta]) 
 manager.execute()
 
 print "Calculation is performed, working on countours"
 
 results = np.array(manager.final())
-angle_plus = results[:, 1]
-angle_minu = results[:, 2]
-print results.shape
+energy = np.array(results[:, 1, :]) 
+
 [mesh_bias, mesh_param] = np.meshgrid(
     biaswindow,
     param_space
 )
- 
-mesh_plus = angle_plus * np.pi
-mesh_minu = angle_minu * np.pi
-
-fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=(20, 23), dpi=1080)
+  
+fig, ax1 = plt.subplots(sharex=True, figsize=(25, 15), dpi=1080)
 plt.xticks(fontsize=30)
 plt.yticks(fontsize=30) 
      
-colors_plus = MaxNLocator(nbins=tick_num).tick_values(-1.0*np.pi, 1.0*np.pi) 
-colors_minu = MaxNLocator(nbins=tick_num).tick_values(-1.0*np.pi, 1.0*np.pi) 
+colors_plus = MaxNLocator(nbins=tick_num).tick_values(np.min(np.min(energy)), np.max(np.max(energy)))  
 
-cf_plus = ax1.contourf(mesh_bias, mesh_param, mesh_plus, cmap=cmap, levels=colors_plus)
-cf_minu = ax2.contourf(mesh_bias, mesh_param, mesh_minu, cmap=cmap, levels=colors_minu)
+cf_plus = ax1.contourf(mesh_bias, mesh_param, energy, cmap=cmap, levels=colors_plus) 
 
 
-cb_plus = fig.colorbar(cf_plus, ax=ax1, shrink=0.9, pad=0.15)    
-cb_minu = fig.colorbar(cf_minu, ax=ax2, shrink=0.9, pad=0.15)    
+cb_plus = fig.colorbar(cf_plus, ax=ax1, shrink=0.9, pad=0.15)     
 
 plt.rc('font', family='serif')
 
 for t in cb_plus.ax.get_yticklabels():
-     t.set_fontsize(20)
-for t in cb_minu.ax.get_yticklabels():
-     t.set_fontsize(20)
+     t.set_fontsize(20) 
 
-ax2.set_xlabel( "Bias $V$" ,fontsize=30); 
+ax1.set_xlabel( "Bias $V$" ,fontsize=30); 
 ax1.set_ylabel( "Zero-bias Level $\\epsilon_0$",fontsize=30); 
-ax2.set_ylabel( "Zero-bias Level $\\epsilon_0$",fontsize=30);
 
 ax1.set_title( "$\\alpha=%.5f$, $\\tau=%.5f$, $\\Gamma=%.5f$, $\\epsilon_0=%.5f$, $\\beta=%.5f$, $U=%.5f$" % (alpha, tau, gamma, levels, beta, capacitive), fontsize=25, y=1.07) 
 
@@ -245,7 +236,7 @@ print "\n Time spent %.6f seconds. \n " % (global_time_end - global_time_start)
 #param_type = args.param
 #ksi = args.ksi
 #zeta = args.zeta
-plt.savefig("perrin_argument_parallel.pdf") 
+plt.savefig("perrin_energy_parallel.pdf") 
 
 
 
